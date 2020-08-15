73d8faeb8eefbd0682b8e1e0cb5005d8085829a2
AbdoMagdy <abdoman46@hotmail.com>
2020-08-16 00:16:23 +0200

    use knowledge from vuex

--- src/views/ui-elements/catalog/KnowledgeBase/KnowledgeBase.vue
+++ src/views/ui-elements/catalog/KnowledgeBase/KnowledgeBase.vue
@@ -1,19 +1,21 @@
 <template lang="html">
   <div>
-    <vs-tabs>
-      <vs-tab v-for="(v, k) in kb" :label="v.label" :key="k">
+    <vs-tabs v-if="knowledge.comments">
+      <vs-tab v-for="(v, k) in knowledge" :label="v.label" :key="k">
         <div class="con-tab-ejemplo">
           <vs-table :data="v.values">
             <template slot="thead">
-              <vs-th class="bg-grey-light rounded-tl"> Key</vs-th>
-              <vs-th class="bg-grey-light rounded-tr"> Value</vs-th>
+              <vs-th class="bg-grey-light rounded-tl w-1/5"> Key</vs-th>
+              <vs-th class="bg-grey-light rounded-tr w-4/5"> Value</vs-th>
             </template>
 
             <template slot-scope="{ data }">
               <vs-tr :key="i" v-for="(value, i) in data">
-                <vs-td :data="value.key"> {{ value.key }} </vs-td>
+                <vs-td :data="value.key" class="break-words">
+                  {{ value.key }}
+                </vs-td>
 
-                <vs-td :data="value.value" class="break-normal">
+                <vs-td :data="value.value" class="break-words">
                   {{ value.value }}
 
                   <template slot="edit">
@@ -27,6 +29,13 @@
               </vs-tr>
             </template>
           </vs-table>
+          <vs-button
+            color="success"
+            class="float-right mt-4"
+            type="filled"
+            @click="editKnowledge"
+            >Save</vs-button
+          >
         </div>
       </vs-tab>
     </vs-tabs>
@@ -35,39 +44,32 @@
 
 <script>
 export default {
-  data: () => ({
-    obj: {
-      1: "hi",
-      2: "hello"
-    },
-    kb: {
-      greetings: {
-        label: "Greetings",
-        values: [
-          {
-            key: "welcome",
-            value: "Hello"
-          },
-          {
-            key: "thankYou",
-            value: "Thanks"
-          }
-        ]
-      },
-      persistantMenu: {
-        label: "Persistan Menu",
-        values: [
-          {
-            key: "welcome",
-            value: "Hello"
-          },
-          {
-            key: "thankYou",
-            value: "Thanks"
-          }
-        ]
-      }
+  data: () => ({}),
+  computed: {
+    knowledge() {
+      return this.$store.getters["catalog/knowledge"];
     }
-  })
+  },
+  methods: {
+    editKnowledge() {
+      console.log(this.$store.getters["catalog/knowledge"].greetings);
+      let knowledgev = {
+        gretings: this.$store.getters["catalog/knowledge"].greetings,
+        comments: this.$store.getters["catalog/knowledge"].comments
+      };
+      console.log(knowledgev);
+      this.$store
+        .dispatch("catalog/editKnowledgeValue", knowledgev)
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }
 };
 </script>
+
+<style scoped>
+table {
+  table-layout: fixed;
+}
+</style>
